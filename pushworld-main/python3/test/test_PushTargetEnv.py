96698390c82ddad804634c06c7a507e7f91ceb67
mikk <lyubin.misha@mail.ru>
2025-11-18 23:41:49 +0300

    add callback

--- pushworld-main/python3/test/test_PushTargetEnv.py
+++ pushworld-main/python3/test/test_PushTargetEnv.py
@@ -11,22 +11,144 @@ from stable_baselines3.common.callbacks import EvalCallback
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '/home/mik/hse/Pushworld/pushworld-main/python3/src/pushworld'))
 from stable_baselines3.common.evaluation import evaluate_policy
 from pushworld.gym_env import PushTargetEnv
-menv = PushTargetEnv("/home/mik/hse/Pushworld/pushworld-main/benchmark/puzzles/level0/all/train", 100)
+from stable_baselines3.common.callbacks import BaseCallback, CallbackList
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+path_to_rep = "/home/mikk/PushWorld/pushworld_rl/pushworld-main/"
+menv = PushTargetEnv(path_to_rep + "benchmark/puzzles/level0/all/train", 100)
 
-eval_env = DummyVecEnv([lambda: PushTargetEnv("/home/mik/hse/Pushworld/pushworld-main/benchmark/puzzles/level0/all/train", 100)])
+eval_env = DummyVecEnv([lambda: PushTargetEnv(path_to_rep + "benchmark/puzzles/level0/all/train", 100)])
 
-model_save_path = "/home/mik/hse/Pushworld/pushworld-main/python3/model/bst"
+model_save_path = path_to_rep + "python3/model/bst"
+
+test_ac = []
+train_ac = []
+
+def create_rgb_video_opencv(data, output_file='rgb_video.avi', fps=10):
+    """
+    Создает видео из RGB данных используя OpenCV
+    """
+    # Получаем размеры первого кадра
+    first_frame = data[0]
+    height, width = first_frame.shape[:2]
+    
+    # Создаем VideoWriter
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(output_file, fourcc, fps, (width, height))
+    
+    for i, rgb_frame in enumerate(data):
+        # Конвертируем RGB в BGR (OpenCV использует BGR)
+        bgr_frame = cv2.cvtColor((rgb_frame * 255).astype(np.uint8), cv2.COLOR_RGB2BGR)
+        
+        # Добавляем текст с номером кадра
+        cv2.putText(bgr_frame, f'Frame: {i}', (10, 30), 
+                   cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        
+        out.write(bgr_frame)
+    
+    out.release()
+    print(f"Video saved as {output_file}")
+
+def test_model(model):
+    test_env = PushTargetEnv(path_to_rep + f"benchmark/puzzles/level0/all/test", 100, to_height = 11, to_width = 11, max_obj = 5, seq = True)
+
+    num_episodes = 200
+    success_count = 0
+    for episode in range(num_episodes):
+
+        obs, _ = test_env.reset()  
+        terminated = False
+        truncated = False
+        episode_rewards = []
+        while not terminated:
+            action, _ = model.predict(obs)  
+
+            obs, reward, terminated, truncated, info = test_env.step(action)
+            episode_rewards.append(reward)
+            if (truncated):
+                break
+        if terminated:
+            create_rgb_video_opencv(test_env.render_video(),path_to_rep + "python3/fotos/" + str(episode) + ".avi")
+            print(1)
+            rgb = test_env.render()
+            success_count += 1
+    print(f"\nРезультаты за {num_episodes} эпизодов:")
+    print(f"Успешных эпизодов: {success_count}")
+    print(f"Процент успеха: {success_count/num_episodes*100:.2f}%")
+    s1 = success_count/num_episodes*100
+    test_ac.append(s1)
+    test_env = PushTargetEnv(path_to_rep + "benchmark/puzzles/level0/all/train", 100, to_height = 11, to_width = 11, max_obj = 5, seq = False)
+
+    num_episodes = 200
+    success_count = 0
+    for episode in range(num_episodes):
+
+        obs, _ = test_env.reset()  
+        terminated = False
+        truncated = False
+        episode_rewards = []
+        while not terminated:
+            action, _ = model.predict(obs)  
+
+            obs, reward, terminated, truncated, info = test_env.step(action)
+            episode_rewards.append(reward)
+            if (truncated):
+                break
+        if terminated:
+            print(1)
+            rgb = test_env.render()
+            success_count += 1
+    print(f"\nРезультаты за {num_episodes} эпизодов:")
+    print(f"Успешных эпизодов: {success_count}")
+    print(f"Процент успеха: {success_count/num_episodes*100:.2f}%")
+    s1 = success_count/num_episodes*100
+    train_ac.append(s1)
+    plt.figure(figsize=(8, 5))
+    plt.plot([i for i in range(len(test_ac))], test_ac)
+    plt.xlabel('Iterations')
+    plt.ylabel('Accuracy')
+    plt.title('Test Accuracy')
+    plt.savefig(path_to_rep + "python3/fotos/test_ac.png")
+    plt.close()
+    plt.figure(figsize=(8, 5))
+    plt.plot([i for i in range(len(train_ac))], train_ac)
+    plt.xlabel('Iterations')
+    plt.ylabel('Accuracy')
+    plt.title('Training Accuracy')
+    plt.savefig(path_to_rep + "python3/fotos/train_ac.png")
+    plt.close()
+    
 
 
+class StatsCallback(BaseCallback):
+    def __init__(self, stats_func, verbose=0):
+        super().__init__(verbose)
+        self.stats_func = stats_func
+    
+    def _on_step(self) -> bool:
+        return True
+    
+    def _on_rollout_end(self) -> None:
+        """Вызывается в конце rollout"""
+        if self.stats_func is not None:
+            self.stats_func(self.model)
+
+# Комбинируем оба callback'а
 eval_callback = EvalCallback(
     eval_env, 
     best_model_save_path=model_save_path,
-    eval_freq=200000,
+    eval_freq=1000,
     n_eval_episodes=10, 
     deterministic=True,
     render=False,
     verbose=1
 )
+
+stats_callback = StatsCallback(stats_func=test_model)
+
+# Объединяем в один callback
+combined_callback = CallbackList([eval_callback, stats_callback])
 
 class CustomCNN(BaseFeaturesExtractor):
     def __init__(self, observation_space, features_dim=128):
@@ -87,12 +209,13 @@ def train_ppo(env):
         device='cuda' if torch.cuda.is_available() else 'cpu'
     )
     
-    model.learn(total_timesteps=30000000, callback=eval_callback)
+    model.learn(total_timesteps=30000000, callback=combined_callback)
     return model
 
 
 model = train_ppo(menv)
-model.save("/home/mik/hse/Pushworld/pushworld-main/python3/model/ppo_custom_model")
+
+model.save(path_to_rep + "python3/model/ppo_custom_model")
 
 # test_env = PushTargetEnv("/home/mik/hse/Pushworld/pushworld-main/benchmark/puzzles/level0/all/train", 100)
 
