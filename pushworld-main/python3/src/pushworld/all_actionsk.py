9feb8d30faed20cb65fc466ba34355378b1f0912
mikkklyubbin <lyubin.misha@mail.ru>
2026-02-12 14:49:52 +0300

    model solver

--- pushworld-main/python3/src/pushworld/all_actionsk.py
+++ pushworld-main/python3/src/pushworld/all_actionsk.py
@@ -21,3 +21,9 @@ def get_check_k_fun(k:int):
                     break
         return max_res
     return check_all_actions
+
+
+def solve_by_model(model, env:PushTargetEnv, k:int):
+    env.restart()
+    for  i in range(k):
+        
